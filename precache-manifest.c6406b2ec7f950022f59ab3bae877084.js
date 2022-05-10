49f950a50252275adb5eac43edc826c0439efa08
DORRA <dorra.soltani.sediri@gmail.com>
2022-05-11 00:11:26 +0200

    Updates

---
+++ precache-manifest.c6406b2ec7f950022f59ab3bae877084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40f5e5cb2a234ccdc9671b793845712",
+    "url": "/First-App-Update/index.html"
+  },
+  {
+    "revision": "48162a19cfe082595984",
+    "url": "/First-App-Update/static/css/main.f412b821.chunk.css"
+  },
+  {
+    "revision": "950bae20d8ee4cf1eb05",
+    "url": "/First-App-Update/static/js/2.8daacdb3.chunk.js"
+  },
+  {
+    "revision": "48162a19cfe082595984",
+    "url": "/First-App-Update/static/js/main.cab17a06.chunk.js"
+  },
+  {
+    "revision": "4889ec4cda13a46d1b6d",
+    "url": "/First-App-Update/static/js/runtime~main.99831f6d.js"
+  }
+]);
